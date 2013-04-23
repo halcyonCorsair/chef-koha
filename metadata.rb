02e87f344386628f77ba68a79e1cf6c260bfef21
chrisfitzpatrick <chrisfitzpatrick@t174.wmu.se>
2013-04-23 14:26:53 +0200

    trying to add depends to metadata

--- metadata.rb
+++ metadata.rb
@@ -6,3 +6,6 @@ long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          "0.0.1"
 
 supports "ubuntu", ">= 12.04"
+depends 'user', '~> 0.3.0'
+depends 'mysql'
+depends 'apache2'
